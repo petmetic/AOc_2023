a6d3569f5293a78e0102b0bd1873be798dde393d
Meta <petmetic@gmail.com>
2023-12-02 16:04:20 +0100

    day1 fix

--- day1/part1.py
+++ day1/part1.py
@@ -5,15 +5,15 @@ def find_digit():
     with open('input.txt') as f:
         lines = f.read().splitlines()
 
-        digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-        numbers_together = []
+    digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    numbers_together = []
 
-        for line in lines:  # line:str, lines: lst[line:str]
-            local_number_digits = []
-            for el in line:  # el: str
-                if el in digits:  # el: str, digits:lst[str]
-                    local_number_digits.append(el)
-            numbers_together.append(local_number_digits)
+    for line in lines:  # line:str, lines: lst[line:str]
+        local_number_digits = []
+        for el in line:  # el: str
+            if el in digits:  # el: str, digits:lst[str]
+                local_number_digits.append(el)
+        numbers_together.append(local_number_digits)
 
     return numbers_together
 
