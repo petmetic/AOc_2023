422ee95c0cc361da3ddb3ad4fbfa4608139870d3
Meta <petmetic@gmail.com>
2023-12-06 19:25:32 +0100

    part 2 day 6

--- day6/part1.py
+++ day6/part1.py
@@ -1,10 +1,10 @@
 from icecream import ic
+import math
 
 
 def parse_input():
-    lines = open("test_input.txt").read().splitlines()
-    # lines = open("input.txt").read().splitlines()
-    ic(lines)
+    # lines = open("test_input.txt").read().splitlines()
+    lines = open("input.txt").read().splitlines()
 
     time, ms = lines[0].split(':')
     tms = list(map(int, ms.split()))
@@ -16,30 +16,32 @@ def parse_input():
 
 
 def calc_dist(time):
-    ic(time)
     ms = []
-    for sec in range(0, time + 1):
+    for sec in range(1, time):
         ms.append(sec)
 
-    ms.reverse()
-    ic(ms)
-    for sec in ms:
-        hold = sec - 1
-        time = sec - hold
-
-        ic(hold, time, sec)
-
-    # hold = time - 1
-    # ic(hold)
-    breakpoint()
+    distances = []
+    for hold in ms:
+        dist = hold *(time - hold)
+        distances.append(dist)
+    return distances
 
 
 def beat_rec_dist(tms, rec_mm):
-    ic(tms, rec_mm)
-    for time in tms:
-        dist = calc_dist(time)
+    all_wins = []
+
+    for idx, time in enumerate(tms):
+        wins = 0
+        distances = calc_dist(time)
+
+        for dist in distances:
+            if dist > rec_mm[idx]:
+                wins += 1
+
+        all_wins.append(wins)
+    return all_wins
 
 
 if __name__ == '__main__':
     tms, rec_mm = parse_input()
-    beat_rec_dist(tms, rec_mm)
+    ic(math.prod(beat_rec_dist(tms, rec_mm)))
